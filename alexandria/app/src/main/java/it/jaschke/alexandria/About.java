9b59df67dd7ee48612e18d1f4032ab91eac3c58c
Gabriel Bernardo Pereira <gabeira@gmail.com>
2015-09-28 01:26:14 -0400

    type: refactor
    Improve the User Interface, including Cardview, Styles and Colors, change the about screen including version.

--- alexandria/app/src/main/java/it/jaschke/alexandria/About.java
+++ alexandria/app/src/main/java/it/jaschke/alexandria/About.java
@@ -1,11 +1,13 @@
 package it.jaschke.alexandria;
 
 import android.app.Activity;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 
 public class About extends Fragment {
@@ -18,13 +20,24 @@ public class About extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_about, container, false);
-        return rootView;
+
+        try {
+            String versionName = String.format(getString(R.string.version_by),
+                    getActivity().getPackageManager().getPackageInfo(getActivity().getPackageName(), 0).versionName);
+            ((TextView) rootView.findViewById(R.id.version)).setText(versionName);
+        }catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
+            return rootView;
     }
 
     @Override
     public void onAttach(Activity activity) {
         super.onAttach(activity);
         activity.setTitle(R.string.about);
+
+
+
     }
 
 }
